27297beb2d4633264593a8da7e4e197ffd6fa208
Semih Güzel <71209213+semihguezel@users.noreply.github.com>
2022-02-15 17:05:21 +0300

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -12,6 +12,7 @@ contract SimpleStorage {
     }
 
     People[] public people;
+    //With mapping attributes of datas can be linked eachother that way you can search by attributes and can find their linked attributed value
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
@@ -22,6 +23,8 @@ contract SimpleStorage {
         return favoriteNumber;
     }
 
+    // In solidty strings are array of bites, to be able store them we have to define their storage type.
+    // There are 2 storage types one of them memory, it stores data only in execution phase, other one is storage where you can store them permenantly
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
